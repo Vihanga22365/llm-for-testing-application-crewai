6b82ecdb466ead75ab2b07c4cbb0ab631e0a4563
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2024-03-07 00:19:34 +0530

    Change GitIgnore

--- 1_Home_Page.py
+++ 1_Home_Page.py
@@ -33,3 +33,26 @@ with col1:
 with col2:
     if st.button('Frontend Testing', key='frontend_button', type="primary", use_container_width=True, help="Click for Frontend Testing"):
         st.switch_page("pages/3_Frontend_Testing.py")
+
+
+import streamlit as st
+import subprocess
+
+def list_installed_libraries():
+    # Use subprocess to execute `pip freeze` and capture the output
+    result = subprocess.run(['pip', 'freeze'], stdout=subprocess.PIPE)
+    # Decode the output from bytes to string
+    installed_libraries = result.stdout.decode('utf-8')
+    return installed_libraries
+
+def main():
+    st.title('Installed Libraries')
+
+    # Button to refresh the list of installed libraries
+    if st.button('Show Installed Libraries'):
+        installed_libraries = list_installed_libraries()
+        # Display the list of installed libraries in the app
+        st.text_area("List of installed libraries:", value=installed_libraries, height=300)
+
+if __name__ == "__main__":
+    main()
