efe080241464f18ec68400761fd3b840af504394
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2024-03-06 22:21:19 +0530

    Change requiremnt.txt

--- pages/4_Backend_Test_Cases_Writing.py
+++ pages/4_Backend_Test_Cases_Writing.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from dotenv import load_dotenv
-# from crewai import Agent, Task, Crew, Process
+from crewai import Agent, Task, Crew, Process
 from langchain_openai import ChatOpenAI
 import os
 import time
