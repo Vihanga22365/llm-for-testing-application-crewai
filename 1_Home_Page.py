2cd0c90476b51cd11a086b72723c30b21ec257b7
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2024-03-07 00:26:03 +0530

    Change GitIgnore

--- 1_Home_Page.py
+++ 1_Home_Page.py
@@ -38,21 +38,28 @@ with col2:
 import streamlit as st
 import subprocess
 
-def list_installed_libraries():
+def get_specific_libraries_versions(libraries):
     # Use subprocess to execute `pip freeze` and capture the output
     result = subprocess.run(['pip', 'freeze'], stdout=subprocess.PIPE)
     # Decode the output from bytes to string
-    installed_libraries = result.stdout.decode('utf-8')
-    return installed_libraries
+    installed_libraries = result.stdout.decode('utf-8').split('\n')
+
+    # Filter for the specific libraries
+    specific_libraries_info = "\n".join([lib for lib in installed_libraries if any(spec_lib in lib for spec_lib in libraries)])
+    return specific_libraries_info
 
 def main():
-    st.title('Installed Libraries')
+    st.title('Check Specific Libraries Versions')
 
-    # Button to refresh the list of installed libraries
-    if st.button('Show Installed Libraries'):
-        installed_libraries = list_installed_libraries()
-        # Display the list of installed libraries in the app
-        st.text_area("List of installed libraries:", value=installed_libraries, height=300)
+    libraries = ['crewai', 'langchain-community', 'langchain', 'langchain-core', 'langchain-openai']
+    
+    if st.button('Show Versions of Specific Libraries'):
+        libraries_versions = get_specific_libraries_versions(libraries)
+        if libraries_versions:
+            # Display the versions of specific libraries
+            st.text_area("Versions of specified libraries:", value=libraries_versions, height=200)
+        else:
+            st.write("None of the specified libraries are installed.")
 
 if __name__ == "__main__":
     main()
